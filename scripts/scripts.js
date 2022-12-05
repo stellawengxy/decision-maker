e4db58a8f3f49dd1437a8e4244882c9d85643018
stellawengxy <xweng@bu.edu>
2022-12-05 15:48:31 -0500

    add coin

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,13 +11,12 @@ const slider = document.querySelector(".slider");
     var slideNumber = 0;
 
     const input = document.getElementById("questionInput");
+
+
+
     //image slider next button
     
-    
-    nextBtn.addEventListener("click", nextSlide);
-    
-    
-    function nextSlide() {
+    nextBtn.addEventListener("click", () => {
         if(input.value.length > 0) {
         
       slides.forEach((slide) => {
@@ -37,7 +36,7 @@ const slider = document.querySelector(".slider");
       slideIcons[slideNumber].classList.add("active");
 
     }
-    };
+    });
 
 
 
@@ -138,3 +137,56 @@ function removeField(minusElement) {
 
 
 
+
+
+
+
+/* coin page */
+
+let heads = 0;
+let tails = 0;
+const coin = document.querySelector(".coin");
+const flipBtn = document.getElementById("flip-button");
+const resetBtn = document.getElementById("reset-button");
+
+
+flipBtn.addEventListener("click", () => {
+let i = Math.floor(Math.random() * 2);
+coin.style.animation = "none";
+if(i){
+  setTimeout(function(){
+    coin.style.animation = "spin-heads 3s forwards";
+  }, 100);
+  heads++;
+
+}
+else{
+  setTimeout(function(){
+    coin.style.animation = "spin-tails 3s forwards";
+  }, 100);
+  tails++;
+
+
+}
+});
+
+
+flipBtn.addEventListener("click", () => {
+let i = Math.floor(Math.random() * 2);
+coin.style.animation = "none";
+if(i){
+  setTimeout(function(){
+    coin.style.animation = "spin-heads 3s forwards";
+  }, 100);
+  heads++;
+
+}
+else{
+  setTimeout(function(){
+    coin.style.animation = "spin-tails 3s forwards";
+  }, 100);
+  tails++;
+
+
+}
+});
