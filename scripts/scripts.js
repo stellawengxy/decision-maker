62fe22b479a1a5438528260f73bcc9cca44c7c8e
stellawengxy <xweng@bu.edu>
2022-12-06 11:35:57 -0500

    changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -84,3 +84,64 @@ function removeField(minusElement) {
 
 
 
+/* coin page */
+
+let heads = 0;
+let tails = 0;
+const coin = document.querySelector(".coin");
+const flipBtn = document.getElementById("flip-button");
+const resetBtn = document.getElementById("reset-button");
+/*let headsChoice = document.querySelector("#head-counts");
+let tailsChoice = document.querySelector("#tails-count");
+*/
+
+
+
+flipBtn.addEventListener("click", () => {
+let i = Math.floor(Math.random() * 2);
+coin.style.animation = "none";
+if(i){
+  setTimeout(function(){
+    coin.style.animation = "spin-heads 3s forwards";
+  }, 100);
+  heads++;
+
+}
+else{
+  setTimeout(function(){
+    coin.style.animation = "spin-tails 3s forwards";
+  }, 100);
+  tails++;
+
+
+}
+});
+
+
+flipBtn.addEventListener("click", () => {
+let i = Math.floor(Math.random() * 2);
+coin.style.animation = "none";
+if(i){
+  setTimeout(function(){
+    coin.style.animation = "spin-heads 3s forwards";
+  }, 100);
+  heads++;
+
+}
+else{
+  setTimeout(function(){
+    coin.style.animation = "spin-tails 3s forwards";
+  }, 100);
+  tails++;
+
+}
+disableButton();
+});
+
+function disableButton(){
+    flipBtn.disabled = true;
+    setTimeout(function(){
+        flipBtn.disabled = false;
+    },3000);
+}
+
